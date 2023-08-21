ee70541e688fde1c01dd7b63189ddf330e05f930
Raimundo Neto <rfmeloneto@gmail.com>
2023-08-20 23:05:20 -0300

    add list

--- lib/DS/list_builder_card.dart
+++ lib/DS/list_builder_card.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import '../transparency/cargos_vagos_ocupados/module/model/positions_model.dart';
 
-Widget listBuilderCard(BuildContext context, Positions model) {
+Widget listBuilderCard(BuildContext context, List<Positions> positionsList) {
   return ListView.builder(
       itemCount: 5,
       itemBuilder: (context, index) {
+        Positions model = positionsList[index]
         return Container(
             padding: const EdgeInsets.all(8.0),
             child: Card(
